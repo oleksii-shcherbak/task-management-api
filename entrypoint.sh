1dd97a03a0766d5bcc77a50665a4826bf3ae7d98
Oleksii Shcherbak <oleksii_shcherbak@icloud.com>
2026-04-03 20:46:38 +0300

    Fix entrypoint: replace pg_isready with python TCP check

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 set -e
 
-until pg_isready -h "${POSTGRES_HOST:-postgres}" -q; do
+until python3 -c "
+import socket, os, sys
+host = os.environ.get('POSTGRES_HOST', 'postgres')
+try:
+    socket.create_connection((host, 5432), timeout=1).close()
+    sys.exit(0)
+except OSError:
+    sys.exit(1)
+" 2>/dev/null; do
   echo "Waiting for postgres..."
   sleep 1
 done
